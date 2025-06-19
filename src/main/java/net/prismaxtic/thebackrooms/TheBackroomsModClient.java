4340988afd15292691d4e62941c9445d3f5083f0
Prismaxtic <maxprismaxtic@gmail.com>
2025-06-19 13:21:57 -0500

    Initial commit

---
+++ src/main/java/net/prismaxtic/thebackrooms/TheBackroomsModClient.java
@@ -0,0 +1,10 @@
+package net.prismaxtic.thebackrooms;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheBackroomsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
